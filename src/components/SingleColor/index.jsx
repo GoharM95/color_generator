c83e85288581a126b6d7ea0522ff74853faefb51
Gohar <gohar.mamunts@gmail.com>
2022-04-20 18:47:38 +0400

    test

--- src/components/SingleColor/index.jsx
+++ src/components/SingleColor/index.jsx
@@ -1,77 +1,100 @@
-// import React from "react";
-// import "./SingleColor.css";
-
-// class SingleColor extends React.Component {
-//   state = { alert: false };
-
-/// ?????
-//   componentDidUpdate(prevProps) {
-//     const { alert } = this.props;
-
-//     if (prevProps.alert !== alert) {
-//       const timeout = setTimeout(() => {
-//         this.setState({ alert: false });
-//       }, 3000);
-//       return () => clearTimeout(timeout);
-//     }
-//   }
-
-//   render() {
-//     const { index, hexColor, rgb, weight, alert } = this.props;
-//     const bcg = rgb.join(",");
-//     const hexValue = `#${hexColor}`;
-
-//     return (
-//       <div
-//         className={`color ${index > 10 && "lightColor"}`}
-//         style={{ backgroundColor: `rgb(${bcg})` }}
-//         onClick={() => {
-//           this.setState({ alert: true });
-//           console.log(this.state.alert);
-//           navigator.clipboard.writeText(hexValue);
-//         }}
-//       >
-//         <p>{weight}%</p>
-//         <p>{hexValue}</p>
-//         {alert && <p className="alert">copied to clipboard</p>}
-//       </div>
-//     );
-//   }
-// }
-
-// export default SingleColor;
-
 import React from "react";
-import { useState, useEffect } from "react";
 import "./SingleColor.css";
 
-const SingleColor = ({ index, hexColor, rgb, weight }) => {
-  const [alert, setAlert] = useState(false);
+class SingleColor extends React.Component {
+  state = { alert: false };
 
-  const bcg = rgb.join(",");
-  const hexValue = `#${hexColor}`;
+  // ?????
+  //   useEffect(() => {
+  //     const timeout = setTimeout(() => {
+  //       setAlert(false);
+  //     }, 300000);
+  //     return () => clearTimeout(timeout);
+  //   }, [alert]);
 
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setAlert(false);
-    }, 3000);
-    return () => clearTimeout(timeout);
-  }, [alert]);
+  // USAGE
+  // componentDidUpdate(prevProps) {
+  //   // Typical usage (don't forget to compare props):
+  //   if (this.props.userID !== prevProps.userID) {
+  //     this.fetchData(this.props.userID);
+  //   }
+  // }
 
-  return (
-    <div
-      className={`color ${index > 10 && "lightColor"}`}
-      style={{ backgroundColor: `rgb(${bcg})` }}
-      onClick={() => {
-        setAlert(true);
-        navigator.clipboard.writeText(hexValue);
-      }}
-    >
-      <p>{weight}%</p>
-      <p>{hexValue}</p>
-      {alert && <p className="alert">copied to clipboard</p>}
-    </div>
-  );
-};
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (prevState.pokemons !== this.state.pokemons) {
+  //     console.log('pokemons state has changed.')
+  //   }
+  // }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { alert } = this.state;
+
+    if (prevState.alert !== alert) {
+      this.timeout = setTimeout(() => {
+        this.setState({ alert: false });
+      }, 3000);
+    }
+  }
+
+  componentWillUnmount() {
+    return () => clearTimeout(this.timeout);
+  }
+
+  render() {
+    const { index, hexColor, rgb, weight, alert } = this.props;
+    const bcg = rgb.join(",");
+    const hexValue = `#${hexColor}`;
+
+    return (
+      <div
+        className={`color ${index > 10 && "lightColor"}`}
+        style={{ backgroundColor: `rgb(${bcg})` }}
+        onClick={() => {
+          this.setState({ alert: true });
+          navigator.clipboard.writeText(hexValue);
+        }}
+      >
+        <p>{weight}%</p>
+        <p>{hexValue}</p>
+        {alert && <p className="alert">copied to clipboard</p>}
+      </div>
+    );
+  }
+}
 
 export default SingleColor;
+
+// import React from "react";
+// import { useState, useEffect } from "react";
+// import "./SingleColor.css";
+
+// const SingleColor = ({ index, hexColor, rgb, weight }) => {
+//   const [alert, setAlert] = useState(false);
+
+//   const bcg = rgb.join(",");
+//   const hexValue = `#${hexColor}`;
+
+//   useEffect(() => {
+//     const timeout = setTimeout(() => {
+//       setAlert(false);
+//     }, 300000);
+//     return () => clearTimeout(timeout);
+//   }, [alert]);
+
+//   return (
+//     <div
+//       className={`color ${index > 10 && "lightColor"}`}
+//       style={{ backgroundColor: `rgb(${bcg})` }}
+//       onClick={() => {
+//         setAlert(true);
+//         navigator.clipboard.writeText(hexValue);
+//       }}
+//     >
+//       <p>{weight}%</p>
+//       <p>{hexValue}</p>
+//       {alert && <p className="alert">copied to clipboard</p>}
+//     </div>
+//   );
+// };
+
+// export default SingleColor;
